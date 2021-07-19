c778ec1c4a94e005ae2ca6d4af8541e972319372
Truc Phan <anhtrucphanit@gmail.com>
2021-07-19 21:21:55 +0700

    Add Babel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const employee = {
-    fullName: 'Truc Phan',
-    position: 'Software Developer',
-    experience: '2 years'
-}
-
-console.log(employee)
+    fullName: "Truc Phan",
+    position: "Software Developer",
+    experience: "2 years",
+};
+const mainEmployeee = {
+    ...mainEmployeee,
+    fullName: "Hilo Hiko",
+    stack: "MERN",
+};
+console.log(mainEmployeee);
+console.log(employee);
